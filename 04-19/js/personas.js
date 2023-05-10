1fa7d136c6bc750d097263f47ee4111067c8cbca
thiago <thiago.gutierrez70@unae.edu.py>
2023-05-10 15:20:10 -0400

    04-19

--- 04-19/js/personas.js
+++ 04-19/js/personas.js
@@ -1,177 +1,149 @@
-//cargar personas
-//listar personas
-//agregar personas
-//editar personas
-//borar personas
+/// cargar personas
+/// listar personas
+// agregar personas
+// editar personas
+// borrar personas
 
 /*
-persona
+Persona
 id
 nombre
 apellido
-cin -> num de documento
-direcciones -> textbox
-fecha de nacimiento
-ciudadad
-fecha_naz
+cin -> nro documento
+direccion -> textbox
+Ciudad
+fecha_nac
 */
-var personas =[];
-var pdatos=[];
-pdatos.push({
-    id:1,
-    nombre:"ivan",
-    apellido:"davalos",
-    cin:"55449165",
-    direccion:"pacu cua",
-    ciudad_id:1,
-    fecha_nac:"2002-11-01"
-});
-pdatos.push({
-    id:2,
-    nombre:"thiago",
-    apellido:"gutierrez",
-    cin:"55117470",
-    direccion:"bernandino caballero",
-    ciudad_id:2,
-    fecha_nac:"2004-04-25"
-});
-//console.log(pdatos)
-function pcargar(){
-    var data=localStorage.getItem("personas");
-    if (!data || data==""){
-        console.log("entro la condicion");
-        let aux=JSON.stringify(pdatos);
-        personas=JSON.parse(aux);
-    }
-    else{
-        personas=JSON.parse(data);
-    }
-    console.log(personas);
-    dibujartabla();
+//console.log(pdatos);
+function pcargar() {
+	cargarPersonas();
+	cargarCiudades();
+	dibujarPTABLA();
+	ciudadCargarSel("ciudad_id", 0);
 }
-function dibujartabla(){
-    console.log("dibujando");
-    var lista= document.getElementById("lista");
-    var thead='<table class="table table-dark "><thead><tr><td>id</td><td>Nombre</td><td>apellido</td><td>cin</td><td>direccion</td><td>ciudad_id</td><td>fecha de nacimiento</td><td colspan="2"><button id="btnew" class="btn btn-primary ">Nuevo</button></td></tr>';
-    var tfoot='</tbody></table>';
-    var tbody="";
-    pdatos.forEach((e) => {
-        tbody=tbody+'<tbody><tr><th>'+
-        e.id+'</th><th>'+
-        e.nombre+'</th><th>'+
-        e.apellido+'</th><th>'+
-        e.cin+'</th><th>'+
-        e.direccion+'</th><th>'+
-        e.ciudad_id+'</th><th>'+
-        e.fecha_nac+'</th><th colspan="2"><button data-id="'+
-        e.id+'" class="btn btn-warning btedit ">Editar</button></th>'+'<th colspan="2"><button data-id="'+
-        e.id+'" class="btn btn-danger btdel ">Borrar</button></th></tr>';
-    });
-    lista.innerHTML = thead+tbody+tfoot;
-    addEventosClk();
+function dibujarPTABLA() {
+	console.log("dibujando");
+	var thtml =
+		"<table class='table'><thead><tr><th>id</th><th>Apellido</th><th>nombre</th><th>Doc.</th><th>DoB</th><th>Ciudad</th><th>Dir</th><th colspan=2 ><a class='btn btn-primary btnew' data-id='-1'>Nuevo</a></th></tr></thead>";
+	thtml = thtml + "<tbody>";
+	var trow = "";
+	personas.forEach((p) => {
+		trow =
+			trow +
+			"<tr><td>" +
+			p.id +
+			"</td><td>" +
+			p.apellido +
+			"</td><td>" +
+			p.nombre +
+			"</td><td>" +
+			p.cin +
+			"</td><td>" +
+			p.fecha_nac +
+			"</td>";
+		//console.log(ciudadById(p.ciudad_id));
+		trow =
+			trow +
+			"<td>" +
+			ciudadById(p.ciudad_id) +
+			"</td><td>" +
+			p.direccion +
+			"</td><td><a class='btn btn-warning btedit' data-id=" +
+			p.id +
+			">Editar</a></td>" +
+			"<td><a class='btn btn-danger btborrar' data-id=" +
+			p.id +
+			">Borrar</a></td>";
+	});
+	thtml = thtml + trow + "</tbody></table>";
+	lista.innerHTML = thtml;
+	addEventosClk();
 }
-
-function addEventosClk(){
-    /*var btnEditar=document.getElementsByClassName("btedit");
-    for(let i=0;i<btnEditar.length;i++){
-        btnEditar[i]=addEventListener("click",clkeditar);
-    }
-    var btnew=document.getElementById("btnew");
-    btnew.addEventListener("click",clknuevo);
-    var btnBorrar=document.getElementsByClassName("btdel");
-    for (let i=0;i<btnBorrar.lengh;i++){
-        btnBorrar[i].addEventListener("click", clkborrar);
-    }
-    */
-    var btnEditar = document.getElementsByClassName("btedit");
-    for (let i = 0; i < btnEditar.length; i++) {
-        btnEditar[i].addEventListener("click", clkeditar);
-    }
-    var btnNuevo = document.getElementById("btnew");
-    btnNuevo.addEventListener("click", clknuevo);
-    var btnBorrar = document.getElementsByClassName("btdel");
-    for (let i = 0; i < btnBorrar.length; i++) {
-        btnBorrar[i].addEventListener("click", clkborrar);
-    }
+function peditar(e) {
+	console.log("editando");
+	eid = e.target.getAttribute("data-id");
+	personas.forEach((item) => {
+		if (item.id == eid) {
+			//console.log(item.fecha_nac);
+			let fecha = new Date();
+			fecha.setDate("2021-12-12");
+			document.getElementById("id").value = item.id;
+			document.getElementById("nombre").value = item.nombre;
+			document.getElementById("apellido").value = item.apellido;
+			document.getElementById("cin").value = item.cin;
+			document.getElementById("ciudad_id").value = item.ciudad_id;
+			document.getElementById("fecha_nac").value = item.fecha_nac;
+			document.getElementById("direccion").value = item.direccion;
+			//console.log(fecha);
+		}
+	});
 }
-
-
-function clkeditar(e){
-    console.log(e.target);
-    eid=e.target.getAttribute('data-id');
-    console.log("clik editar");
-    pdatos.forEach((item)=>{
-        if (item.id==eid){
-            console.log(item);
-            document.getElementById("id").value=item.id;
-            document.getElementById("nombre").value=item.nombre;
-            document.getElementById("apellido").value=item.apellido;
-            document.getElementById("cin").value=item.cin;
-            document.getElementById("direccion").value=item.direccion;
-            document.getElementById("ciudad_id").value=item.ciudad_id;
-            document.getElementById("fecha_nac").value=item.fecha_nac;
-        }
-    });
+function pborrar(e) {
+	console.log("borrando");
 }
-
-function clknuevo(){
-    console.log("nuevo");
-    document.getElementById("id").value =-1;
-    document.getElementById("nombre").value ="";
-    document.getElementById("apellido").value ="";
-    document.getElementById("cin").value ="";
-    document.getElementById("direccion").value ="";
-    document.getElementById("ciudad_id").value ="";
-    document.getElementById("fecha_nac").value ="";
+function pnuevo() {
+	console.log("nuevo");
+	document.getElementById("id").value = -1;
+	document.getElementById("nombre").value = "";
+	document.getElementById("apellido").value = "";
+	document.getElementById("cin").value = "";
+	document.getElementById("fecha_nac").value = "";
+	document.getElementById("direccion").value = "";
 }
-
-function clkborrar(e){
-    console.log("borrando");
-    eid=e.target.getAttribute("data-id");
-    pdatos.forEach((item,idx)=>{
-        if (item.id==eid){
-            pdatos.splice(idx,1);
-            dibujartabla();
-            
-        }
-    });
+function addEventosClk() {
+	console.log("agregagndo eventos");
+	var btnEditar = document.getElementsByClassName("btedit");
+	for (let i = 0; i < btnEditar.length; i++) {
+		btnEditar[i].addEventListener("click", peditar);
+	}
+	var btnuevo = document.getElementsByClassName("btnew");
+	btnuevo[0].addEventListener("click", pnuevo);
+	var btnborrar = document.getElementsByClassName("btborrar");
+	for (let i = 0; i < btnborrar.length; i++) {
+		btnborrar[i].addEventListener("click", pborrar);
+		// console.log(btnEditar[i]);
+	}
+	console.log("Los eventos fueron cargados");
 }
-
-function pguardar(){
-    var gid = document.getElementById("id").value;
-    var gnombre = document.getElementById("nombre").value;
-    var gapellido = document.getElementById("apellido").value;
-    var gcin = document.getElementById("cin").value;
-    var gdireccion = document.getElementById("direccion").value;
-    var gciudad_id = document.getElementById("ciudad_id").value;
-    var gfecha_nac = document.getElementById("fecha_nac").value;
-    console.log(gid+" - "+gnombre+" - "+gapellido+" - "+gcin+" - "+gdireccion+" - "+gciudad_id+" - "+gfecha_nac);	
-    if (gid==-1){
-    //console.log("nuevo item");
-    gid=pdatos[pdatos.length-1].id+1;
-    pdatos.push({ 
-        id: gid, 
-        nombre: gnombre,
-        apellido: gapellido,
-        cin: gcin,
-        direccion: gdireccion,
-        ciudad_id: gciudad_id,
-        fecha_nac: gfecha_nac
-    });
-    console.log("agregar")
-    }
-    else{
-        console.log("editar item")
-        pdatos.forEach((e) =>{
-                if (gid==e.id){
-                    e.nombre=gnombre;
-                    e.apellido=gapellido;
-                    e.cin=gcin;
-                    e.direccion=gdireccion;
-                    e.ciudad_id=gciudad_id;
-                    e.fecha_nac=gfecha_nac;
-                }
-            })
-    }
-    dibujartabla();
-    }
+function pguardar() {
+	var gid = document.getElementById("id").value;
+	var gnombre = document.getElementById("nombre").value;
+	var gapellido = document.getElementById("apellido").value;
+	var gcin = document.getElementById("cin").value;
+	var gfecha_nac = document.getElementById("fecha_nac").value;
+	var gdireccion = document.getElementById("direccion").value;
+	var gciudad_id = document.getElementById("ciudad_id").value;
+	console.log(gid + " - " + gnombre);
+	if (gid == -1) {
+		console.log("nuevo item");
+		gid = personas[personas.length - 1].id + 1;
+		personas.push({
+			id: gid,
+			apellido: gapellido,
+			nombre: gnombre,
+			cin: gcin,
+			direccion: gdireccion,
+			ciudad_id: gciudad_id,
+			fecha_nac: gfecha_nac,
+		});
+	} else {
+		console.log("editar item");
+		personas.forEach((e) => {
+			if (gid == e.id) {
+				e.nombre = gnombre;
+				e.apellido = gapellido;
+				e.cin = gcin;
+				e.fecha_nac = gfecha_nac;
+				e.ciudad_id = gciudad_id;
+				e.direccion = gdireccion;
+			}
+		});
+	}
+	ppersistir();
+	console.log(personas)
+	dibujarPTABLA();
+}
+ function ppersistir() {
+		localStorage.setItem("personas", JSON.stringify(personas));
+ }  
+ window.addEventListener("load", pcargar);
